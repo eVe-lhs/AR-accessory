8472cef1342d2d9ceb53a4e142106b0e7319d3ce
Lin Htet Swe <linhtetswe2012@gmail.com>
2025-01-16 21:21:32 +0900

    updated port scanning error

--- app.py
+++ app.py
@@ -152,4 +152,5 @@ def delete_accessory(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Get the port Render provides
+    app.run(host="0.0.0.0", port=port, debug=True)
